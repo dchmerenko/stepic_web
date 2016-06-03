72e0d2941d7ed32a55be933582da9da01b22ca76
dchmerenko <dchmerenko@gmail.com>
2016-06-03 14:19:48 +0300

    init.sh

---
+++ clone_link.sh
@@ -0,0 +1,4 @@
+# Command for cloning project with directory structure from github and link script
+
+git clone https://github.com/dchmerenko/stepic_web.git /home/box/web
+bash /home/box/web/init.sh
---
+++ init.sh
@@ -0,0 +1,9 @@
+# linking conf files and restarting nginx and gunicorn
+
+# ln - make links between files
+# ln [OPTION]... [-T] TARGET LINK_NAME   (1st form)
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enebled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
